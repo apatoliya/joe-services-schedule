5fca96d6d265f3eb6f902de3b30451274de14612
ridhdhi patel <ridhdhi@ridhdhis-MacBook-Pro.local>
2019-02-06 10:50:25 -0600

    added cassandra schema and seed the DB

---
+++ database/seedscripts/cass-feed.js
@@ -0,0 +1,464 @@
+
+USE schedule;
+
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush, playerrec, pass, rush, rec)
+    VALUES
+        (1,
+         'vs',
+        'Arizona',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+         1,
+        '28-Oct-2018',
+        'Cardinals',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/ari.png&h=40&w=40',
+        '34-0',
+        '1-0',
+        'W',
+        'http://www.espn.com/nfl/team/_/name/ari/arizona-cardinals',
+        'Goff',
+        'Gurley II',
+        'Cooks',
+        270,
+        141,
+        111 );
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        2,
+        '@',
+        'Atlanta',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        2,
+        '08-Dec-2018',
+        'Falcons',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/atl.png&h=40&w=40',
+        '23-20',
+        '2-0',
+        'W',
+        'http://www.espn.com/nfl/team/_/name/atl/atlanta-falcons',
+        'Goff',
+        'Brown',
+        'Woods',
+        325,
+        128,
+        119);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        3,
+        'vs' ,
+        'Denver',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        3,
+        '16-Sep-2018',
+        'Broncos',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/den.png&h=40&w=40',
+        '30-23',
+        '2-1',
+        'L',
+        'http://www.espn.com/nfl/team/_/name/den/denver-broncos',
+        'Goff',
+        'Gurley II',
+        'Woods',
+        219,
+        207,
+        98);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        4,
+         'vs' ,
+        'Green Bay',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        4,
+        '11-Sep-2018',
+        'Packers',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/gb.png&h=40&w=40',
+         'W 14-0',
+        '3-1',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/gb/green-bay-packers',
+        'Goff',
+        'Gurley II',
+        'Woods',
+        287,
+        62,
+        172);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        5,
+         '@' ,
+        'Kansas City',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        5,
+        '10-Nov-2018',
+        'Chiefs',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/kc.png&h=40&w=40',
+         '39-10',
+        '4-1',
+        'W',
+        'http://www.espn.com/nfl/team/_/name/kc/kansas-city-chiefs',
+        'Goff',
+        'Gurley II',
+        'Kupp',
+        366,
+        64,
+        140);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        6,
+         'vs' ,
+        'San Francisco',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        6,
+        '09-Nov-2018',
+        '49ers',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/sf.png&h=40&w=40',
+         '35-23',
+        '5-1',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/sf/san-francisco-49ers',
+        'Goff',
+        'Brown',
+        'Woods',
+        425,
+        129,
+        201);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        7,
+         '@' ,
+        'Washington',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        7,
+        '18-Nov-2018',
+        'Redskins',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/wsh.png&h=40&w=40',
+         '30-23',
+        '5-2',
+         'L',
+        'http://www.espn.com/nfl/team/_/name/wsh/washington-redskins',
+        'Goff',
+        'Gurley II',
+        'Cooks',
+        360,
+        66,
+        125);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        8,
+         'vs' ,
+        'Jacksonville',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        8,
+        '12-Dec-2018',
+        'Jaguars',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/jax.png&h=40&w=40',
+         '38-31',
+        '6-2',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/jax/jacksonville-jaguars',
+        'Goff',
+        'Gurley II',
+        'Woods',
+        192,
+        68,
+        95);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        9,
+         '@' ,
+        'Buffalo',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        9,
+        '13-Nov-2018',
+        'Bills',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/buf.png&h=40&w=40',
+         '45-35',
+        '6-3',
+         'L',
+        'http://www.espn.com/nfl/team/_/name/buf/buffalo-bills',
+        'Goff',
+        'Gurley II',
+        'Woods',
+        274,
+        119,
+        224);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        10,
+        '',
+        '',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        10,
+        'BYE WEEK',
+        'BYE WEEK',
+        'https://cdn.shocho.co/sc-site/brandPages/Grills/Jeeva_logo_small_-_transparent_PNG_file_400x400-1.png',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        0,
+        0,
+        0);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        11,
+         '@',
+        'Indianapolis',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        11,
+        '16-Nov-2018',
+        'Colts',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/ind.png&h=40&w=40',
+         '54-51',
+        '7-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/ind/indianapolis-colts',
+        'Goff',
+        'Gurley II',
+        'Kupp',
+        184,
+        172,
+        110);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        12,
+         'vs' ,
+        'Baltimore',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        12,
+        '11-Sep-2018',
+        'Ravens',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/bal.png&h=40&w=40',
+         '30-16',
+        '8-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/bal/baltimore-ravens',
+        'Goff',
+        'Brown',
+        'Cooks',
+        230,
+        241,
+        189);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        13,
+         'vs',
+        'Miami',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        13,
+        '24-Nov-2018',
+        'Dolphins',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/mia.png&h=40&w=40',
+         '29-27',
+        '9-3',
+        'W',
+        'http://www.espn.com/nfl/team/_/name/mia/miami-dolphins',
+        'Goff',
+        'Gurley II',
+        'Cooks',
+        367,
+        63,
+        170);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        14,
+         '@' ,
+        'Cleveland',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        14,
+        '30-Nov-2018',
+        'Browns',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/cle.png&h=40&w=40',
+         '33-13',
+        '10-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/cle/cleveland-browns',
+        'Goff',
+        'Brown',
+        'Woods',
+        218,
+        216,
+        144);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        15,
+         'vs' ,
+        'Cincinnati',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        15,
+        '15-Dec-2018',
+        'Bengals',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/cin.png&h=40&w=40',
+         '35-23',
+        '11-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/cin/cincinnati-bengals',
+        'Goff',
+        'Gurley II',
+        'Gurley II',
+        242,
+        269,
+        210);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        16,
+         'vs' ,
+        'Los Angeles',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        16,
+        '08-Oct-2018',
+        'Chargers',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/lac.png&h=40&w=40',
+         '23-20',
+        '12-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/lac/los-angeles-chargers',
+        'Goff',
+        'Gurley II',
+        'Cooks',
+        154,
+        150,
+        228);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        17,
+         'vs' ,
+        'Detroit',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        17,
+        '26-Oct-2018',
+        'Lions',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/det.png&h=40&w=40',
+         '31-3',
+        '13-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/det/detroit-lions',
+        'Goff',
+        'Brown',
+        'Woods',
+        312,
+        130,
+        246);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        18,
+         'vs' ,
+        'New England',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        18,
+        '25-Jan-2019',
+        'Patriots',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/ne.png&h=40&w=40',
+         'W 33-3',
+        '14-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/ne/new-england-patriots',
+        'Goff',
+        'Gurley II',
+        'Woods',
+        312,
+        130,
+        246);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        19,
+         'vs' ,
+        'New York',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        19,
+        '31-Jan-2019',
+        'Jets',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/nyj.png&h=40&w=40',
+         '43-3',
+        '15-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/nyj/new-york-jets',
+        'Goff',
+        'Gurley II',
+        'Cooks',
+        312,
+        130,
+        246);
+    
+INSERT INTO stats (id, vs, city, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush,playerrec,pass, rush, rec)
+    VALUES
+    (
+        20,
+         'vs' ,
+        'Pittsburgh',
+        'Los Angeles Rams',
+        'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+        20,
+        '7-Feb-2019',
+        'Steelers',
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/pit.png&h=40&w=40',
+         '13-3',
+        '16-3',
+         'W',
+        'http://www.espn.com/nfl/team/_/name/pit/pittsburgh-steelers',
+        'Goff',
+        'Brown',
+        'Kupp',
+        312,
+        130,
+        246);
+    
+
+
+ 
