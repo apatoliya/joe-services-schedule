e0dbdd6ae958071fa947f288035c9da477c0ba61
ridhdhi patel <ridhdhi@ridhdhis-MacBook-Pro.local>
2019-02-11 22:04:07 -0600

    added second version seed script for cassandra

--- database/seedscripts/test1.js
+++ database/seedscripts/test1.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 let finaldata = [];
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < 1000000; i++) {
-  const ramSchedule = `${i}, ${faker.address.city()}, '08-dec-2018', ${faker.internet.url()}, ${faker.random.word()}, ${faker.image.abstract()}, ${faker.random.number()}, ${faker.random.word()}, ${faker.random.word()}, ${faker.random.word()}, ${faker.random.number()}, ${faker.random.number()}, ${faker.random.number()},${faker.random.word()},${faker.image.abstract()}, 'vs', ${i}, ${faker.random.word()}, 'W'`;
+  const ramSchedule = `${i}, ${faker.address.city()}, '08-dec-2018', ${faker.internet.url()}, ${faker.random.word()}, ${faker.image.abstract()}, ${faker.random.number()}, ${faker.random.word()}, ${faker.random.word()}, ${faker.random.word()}, ${faker.random.number()}, ${faker.random.word()}, ${faker.random.number()},${faker.random.word()},${faker.image.abstract()}, 'vs', ${i}, ${faker.random.word()}, 'W'`;
   finaldata.push([ramSchedule]);
 }
 console.log('this is finaldata length', finaldata.length); 
