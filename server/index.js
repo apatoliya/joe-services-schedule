7b7cee562d9ae826cc49871c1431b42a9f6265b9
ridhdhi patel <ridhdhi@ridhdhis-MacBook-Pro.local>
2019-02-13 13:57:29 -0600

    added CRUD routes for mysql DB

--- server/index.js
+++ server/index.js
@@ -43,3 +43,59 @@ const port = process.env.PORT || 3007;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+
+// CRUD operation
+
+
+// post
+app.post('/espn/schedules', (req, res) => {
+  const {
+    id, vs, team, teamlogo, week, date, opponent, opponentlogo, result, winloss, wl, link, playerpass, playerrush, playerrec, pass, rush, rec,
+  } = req.body;
+  const body = {
+    id: 21,
+    vs: ' vs ',
+    city: 'okc',
+    team: 'Thunder',
+    teamlogo: 'http://icons.iconarchive.com/icons/astahrr/nfl/48/Rams-icon.png',
+    week: 21,
+    date: '7-Feb-2019',
+    opponent: 'Steelers',
+    opponentlogo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/pit.png&h=40&w=40',
+    result: ' 13-3',
+    winloss: '16-3',
+    wl: ' W',
+    link: 'http://www.espn.com/nfl/team/_/name/pit/pittsburgh-steelers',
+    playerpass: 'Goff',
+    playerrush: 'Brown',
+    playerrec: 'Kupp',
+    pass: 312,
+    rush: 130,
+    rec: 246,
+  };
+  database.insert(body).from('stats').then(() => {
+    res.send('Successfully post data into DB!!!');
+  });
+});
+// update
+
+app.put('/espn/schedules', (req, res) => {
+  database.where({
+    id: 21,
+  }).update({
+    city: 'Oklahoma City',
+  }).from('stats').then(() => {
+    res.send('Successfully update the record into DB!!!!');
+  });
+});
+
+// delete
+
+app.delete('/espn/schedules', (req, res) => {
+  database.where({
+    id: 21,
+  }).del().from('stats').then(() => {
+    res.send('Successfully delete the record into DB!!!!');
+  });
+});
